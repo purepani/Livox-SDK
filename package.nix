8ee6e5fdc0a59c8b60ba23295f2b6a1a0e0a83c0
purepani <pani0028@umn.edu>
2024-02-08 15:03:00 -0600

    simplifies package

--- package.nix
+++ package.nix
@@ -7,24 +7,23 @@ clangStdenv.mkDerivation {
   pname = "Livox-SDK";
   version = "2.3.0";
   src = ./.;
-  hardeningDisable = ["all"];
   nativeBuildInputs = [
     cmake
   ];
-  configurePhase = ''
-    cd build
-    cmake ..
-  '';
+  #configurePhase = ''
+  #  cd build
+  #  cmake ..
+  #'';
 
-  buildPhase = ''
-    make
-  '';
+  #buildPhase = ''
+  #  make
+  #'';
 
-  installPhase = ''
-    mkdir -p $out/bin
-    mkdir -p $out/lib
-    mv sdk_core/liblivox_sdk_static.a $out/lib
-    mv sample/ $out/bin
-    mv sample_cc/ $out/bin
-  '';
+  #installPhase = ''
+  #  mkdir -p $out/bin
+  #  mkdir -p $out/lib
+  #  mv sdk_core/liblivox_sdk_static.a $out/lib
+  #  mv sample/ $out/bin
+  #  mv sample_cc/ $out/bin
+  #'';
 }
